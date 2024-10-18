f4d0fa93f52d3cca1fb9741bc3a7118b7d3409e7
Luís Condados <condadoslgpc@gmail.com>
2024-10-18 10:09:03 -0400

    update

--- python/download_images.py
+++ python/download_images.py
@@ -2,7 +2,7 @@
 from pexel_downloader import PexelDownloader
 
 if __name__ == '__main__':
-    downloader = PexelDownloader(api_key="ta6EGYFHOxtstcudGptbyxksShcvdc8X7sZz96rKuwaY8rq3AXLnPHqJ")
+    downloader = PexelDownloader(api_key="YOUR-PEXELS-API-KEY")
     downloader.download_images(query='space',
                                num_images=1000,
                                save_directory='../images',
